16e9df17fd83ed042c88c7b129316caba6a01de0
triton-lrdf <malik.jabou@etu.univ-lyon1.fr>
2025-07-20 16:02:42 +0100

    fait trop cho flemme de bosser...

---
+++ header/decor.h
@@ -0,0 +1,20 @@
+
+
+#include "case.h"
+using namespace std ;
+
+class Decor : Case 
+{
+
+
+private:
+    /* data */
+
+public:
+    Decor(/* args */);
+    Decor(string type) ;
+    bool marcherSur () {return false ;}
+    ~Decor();
+
+
+};
