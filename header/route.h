4775a322c53ce8c75db87f53f573a484c3a898f5
triton-lrdf <malik.jabou@etu.univ-lyon1.fr>
2025-08-15 11:03:55 +0200

    j'ai vite fait entamer la theorie de la classe route g trop la flemme de continuer donc voila

--- header/route.h
+++ header/route.h
@@ -7,17 +7,26 @@ class Route
 private:
 
     vector<Chemin*> laRoute ;
+    int len ;
     /* data */
 public:
 
-    
-    void ajouterChem(Chemin* leChem, int position, char direction) ; // le int c 1ou -1 je voulais faire un shortInt mais g pas trouver le char c NSEO pour tourner
+
+    void ajouterChem(Chemin* leChem, int position, char direction) ; // le int c 1 ou len le char c NSEO pour tourner
     void retirerChem (int pos) ; // ici pareil on peut pas mettre ou enlever de chemins n'importe ou on peut que au debut ou a la fin 
+    vector<Chemin*> couperRoute(int pos, int dir) ; // donc en gros la on va prendre la route a la pos donnée et on va garder que ceux ans la dir donnée
+    // ceux qu'on va les retourner en leur metant une nouvelle route qui sera un nom en mode ancienement route de truc
+
     void promouvoir(int pos) ;
     void promouvoir(Chemin* leReuxElu) ; 
+    
     Route(/* args */);
     Route(string nom) ;
     ~Route();
+
+    int length() {return len ;}
+    Chemin* debut () ;
+    Chemin* fin () ;
 
 };
 
